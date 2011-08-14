a21eda3a9759e27717cc06e54ca13e09f651d202
Steve Farrell <steve@farrell.org>
2011-08-13 17:18:16 -0700

    none

--- app/models/game.rb
+++ app/models/game.rb
@@ -9,6 +9,7 @@ class Game < ActiveRecord::Base
                       :too_short => "cannot be missing",
                       :too_long => "at most 255 characters")
   before_destroy :check_destroyable
+  validates_numericality_of :num_turns, :winning_turn_id
   
   def check_destroyable
     raise "cannot delete a game with turns" if turns.any?
