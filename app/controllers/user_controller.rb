eb90f0102558d668040794ade06b84fd6ae6622d
Steve Farrell <steve@farrell.org>
2011-08-14 19:37:51 -0700

    add update method to user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,6 +7,12 @@ class UserController < ApplicationController
     render :json => User.find(params[:id])
   end
 
+  def update
+    user = User.find(params[:id])
+    user.update_attributes!(params[:story])
+    render :json => user
+  end
+
   def create
     user = User.new(params[:user])
     user.save!
