7e35f0bd221be7878e5923f479139c31ccf5631a
Steve Farrell <steve@farrell.org>
2011-08-22 23:55:27 -0700

    s/right|wrong/thumbs up|down/

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 RESULT = { :none => 0, :right => 1, :wrong => -1 }
-RESULT_HUMAN = { 1 => "right", -1 => "wrong" }
+RESULT_HUMAN = { 1 => [0xe00e].pack('U'), -1 => [0xe421].pack('U') }
 
 # Initialize the rails application
 EmojiCharades::Application.initialize!
